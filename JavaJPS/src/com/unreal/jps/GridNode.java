38903cc9a5982aaf24e9c1bef148e48b51d927da
Shane <Shane@192.168.1.120>
2013-09-14 14:34:58 -0700

    Started implementing the Nodes and their class system.

---
+++ JavaJPS/src/com/unreal/jps/GridNode.java
@@ -0,0 +1,16 @@
+package com.unreal.jps;
+
+/**
+ *
+ * @author Shane
+ */
+public class GridNode extends Node{
+
+    private int myX, myY;
+    
+    @Override
+    public int getFScore() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- JavaJPS/src/com/unreal/jps/Node.java
+++ JavaJPS/src/com/unreal/jps/Node.java
@@ -4,6 +4,43 @@ package com.unreal.jps;
  *
  * @author Shane
  */
-public class Node {
+public abstract class Node implements Comparable{
     
+    private Node myParentNode;
+    
+    /**
+     * Gets this nodes FScore
+     * @return the FScore
+     */
+    public abstract int getFScore();
+    
+    /**
+     * Gets this node's parent
+     * @return the parent node
+     */
+    public Node getParent()
+    {
+        return this.myParentNode;
+    }//end getParent
+    
+    /**
+     * Sets this node's parent
+     * @param parentNode the parent node
+     */
+    public void setParent(Node parentNode)
+    {
+        this.myParentNode = parentNode;
+    }//end setParent
+    
+    @Override
+    public int compareTo(Object t) {
+        if(!(t instanceof Node))
+            return 0;
+        Node other = (Node)t;
+        if(getFScore() < other.getFScore())
+            return -1;
+        else if(getFScore() > other.getFScore())
+            return 1;
+        else return 0;
+    } 
 }
