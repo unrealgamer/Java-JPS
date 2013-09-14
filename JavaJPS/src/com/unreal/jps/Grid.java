09d7e00a3ab77ff00146a7ad8faf4f09317b99ab
Shane <Shane@192.168.1.120>
2013-09-14 15:06:04 -0700

    Started some more grid stuff...

---
+++ JavaJPS/src/com/unreal/jps/Grid.java
@@ -0,0 +1,16 @@
+package com.unreal.jps;
+
+/**
+ *
+ * @author Shane
+ */
+public class Grid {
+    
+    private Node[][] myNodes;
+    
+    public Grid(int width, int height)
+    {
+        this.myNodes = new Node[width][height];
+    }
+    
+}
--- JavaJPS/src/com/unreal/jps/GridNode.java
+++ JavaJPS/src/com/unreal/jps/GridNode.java
@@ -8,6 +8,31 @@ public class GridNode extends Node{
 
     private int myX, myY;
     
+    public GridNode(int x, int y)
+    {
+        this.myX = x;
+        this.myY = y;
+    }
+    
+    public GridNode(int x, int y, Node parent)
+    {
+        this.myX = x; 
+        this.myY = y;
+        this.setParent(parent);
+    }
+    
+    public void setLocation(int x, int y) {
+        this.myX = x; this.myY = y;
+    }
+    
+    public int getX() {
+        return this.myX;
+    }
+    
+    public int getY() {
+        return this.myY;
+    }
+    
     @Override
     public int getFScore() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
