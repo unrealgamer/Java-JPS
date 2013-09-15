a3ba09bfeb7a7149070ecf9a2eb89a41e904c5de
Adrian <Adrian@192.168.2.11>
2013-09-14 17:18:12 -0700

    finished-ish findForcedNeighbors method blew nose

--- JavaJPS/src/com/unreal/jps/PathFinder.java
+++ JavaJPS/src/com/unreal/jps/PathFinder.java
@@ -9,26 +9,49 @@ package com.unreal.jps;
  *
  * @author Adrian
  */
-public class PathFinder
-{
+public class PathFinder {
 
-/*
-	public static GridNode[] findForcedNeighbor(GridNode origin, Vector2 direction)
-	{
-		//Grid myGrid = Program.getGrid();
-		//GridNode[] pathNodes = new GridNode[2];
-		if (direction.x == 0)
-		{
-			//Direction is vertical
-			//if myGrid.getCell(origin.x){}
-		}
-		else if (direction.y == 0)
-		{
-			//Direction is horizontal
-		}
+    public static GridNode[] findForcedNeighbors(Grid myGrid, GridNode origin, Vector2 direction) {
+        GridNode[] pathNodes = new GridNode[2];
+        int numNodes = 0;
+        if (direction.x == 0) {
+            //Direction is vertical
+            if (!myGrid.getNode(origin.x + 1, origin.y).isPassable() && myGrid.getNode(origin.x + 1, origin.y + direction.y).isPassable()) {
+                //found forced neighbor to the right and ahead
+                pathNodes[numNodes] = myGrid.getNode(origin.x + 1, origin.y + direction.y);
+                numNodes++;
+            }
+            if (!myGrid.getNode(origin.x - 1, origin.y).isPassable() && myGrid.getNode(origin.x - 1, origin.y + direction.y).isPassable()) {
+                //found forced neighbor to the left and ahead
+                pathNodes[numNodes] = myGrid.getNode(origin.x - 1, origin.y + direction.y);
+                numNodes++;
+            }
+        } else if (direction.y == 0) {
+            //Direction is horizontal
+            if (!myGrid.getNode(origin.x, origin.y + 1).isPassable() && myGrid.getNode(origin.x + direction.x, origin.y + 1).isPassable()) {
+                //Found forced neighbor above and ahead
+                pathNodes[numNodes] = myGrid.getNode(origin.x + direction.x, origin.y + 1);
+                numNodes++;
+            }
+            if (!myGrid.getNode(origin.x, origin.y - 1).isPassable() && myGrid.getNode(origin.x + direction.x, origin.y - 1).isPassable()) {
+                //Found forced neighbor below and ahead
+                pathNodes[numNodes] = myGrid.getNode(origin.x + direction.x, origin.y - 1);
+                numNodes++;
+            }
+        } else {
+            //Direction is diagonal
+            if (!myGrid.getNode(origin.x - direction.x, origin.y) && myGrid.getNode(origin.x - direction.x, origin.y + direction.y)) {
+                //found forced neighbor to side A
+                pathNodes[numNodes] = myGrid.getNode(origin.x - direction.x, origin.y + direction.y);
+                numNodes++;
+            }
+            if (!myGrid.getNode(origin.x, origin.y - direction.y) && myGrid.getNode(origin.x + direction.x, origin.y - direction.y)) {
+                //found forced neighbor to side B
+                pathNodes[numNodes] = myGrid.getNode(origin.x + direction.x, origin.y - direction.y);
+                numNodes++;
+            }
+        }
+        return pathNodes;
+    }
 
-		return pathNodes;
-	}
-
-*/
 }
