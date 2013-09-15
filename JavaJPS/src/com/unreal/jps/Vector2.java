c1b33adad3f04798a267afebaa8c58478ec97650
Adrian <Adrian@192.168.2.11>
2013-09-14 17:28:29 -0700

    Vector2 class added

---
+++ JavaJPS/src/com/unreal/jps/Vector2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unreal.jps;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Vector2 {
+
+    public int x, y;
+
+    public Vector2(int inX, int inY) {
+        x = inX;
+        y = inY;
+    }
+}
